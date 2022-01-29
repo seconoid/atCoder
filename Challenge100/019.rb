efd11adfc0b5be918397dc8e63bc344b265c7d81
seconoid <seconsecond@gmail.com>
2022-01-30 00:17:30 +0900

    AC #019

--- Challenge100/019.rb
+++ Challenge100/019.rb
@@ -14,14 +14,23 @@ LIST_K = M.times.map do |i|
 end
 
 def resolve(d, n, m, list_d, list_k)
-  shops = [0, list_d].flatten
+  shops = [0, list_d.sort].flatten
   delivery_list = list_k
 
   delivery_list.sum { |deliver_to| search_shortest_path(deliver_to, shops, d) }
 end
 
 def search_shortest_path(deliver_to, shops, d)
-  shops.map { |shop| [(shop - deliver_to).abs, (shop + d - deliver_to).abs].min }.min
+  bsearched_shop_index = shops.bsearch_index { |x| x >= deliver_to } || 0
+  bsearched_shop = shops[bsearched_shop_index]
+  bsearched_shop_before = shops[bsearched_shop_index - 1]
+
+  [
+    (bsearched_shop - deliver_to).abs,
+    (bsearched_shop + d - deliver_to).abs,
+    (bsearched_shop_before - deliver_to).abs,
+    (bsearched_shop_before + d - deliver_to).abs
+  ].min
 end
 
 puts resolve(D, N, M, LIST_D, LIST_K)
